de9e3806a4cf74cf22d63c078815e672aa5ef7ec
Adrian Tegelind <adriantegelind@outlook.com>
2025-04-14 15:52:54 +0200

    Added unit tests for the function get_user_by_email.

---
+++ backend/test/unit/test_usercontroller.py
@@ -0,0 +1,39 @@
+
+import pytest
+import unittest.mock as mock
+from unittest.mock import patch
+from src.controllers.usercontroller import UserController
+
+
+@pytest.fixture
+def user1():
+    return {'firstName': 'Jane', 'lastName': 'Doe', 'email': 'jane.doe@email.com'}
+
+@pytest.fixture
+def user2():
+    return {'firstName': 'Janet', 'lastName': 'Doeg', 'email': 'jane.doe@email.com'}
+
+
+def test_get_user_by_email_1_match():
+    mockedDAO = mock.MagicMock()
+    mockedDAO.find.return_value = [user1]
+    uc = UserController(dao=mockedDAO)
+
+    assert uc.get_user_by_email(email='jane.doe@email.com') == user1
+
+
+def test_get_user_by_email_multiple_matches():
+    mockedDAO = mock.MagicMock()
+    mockedDAO.find.return_value = [user1, user2]
+    uc = UserController(dao=mockedDAO)
+
+    assert uc.get_user_by_email(email='jane.doe@email.com') == user1
+
+
+def test_get_user_by_email_0_matches():
+    mockedDAO = mock.MagicMock()
+    mockedDAO.find.return_value = []
+    uc = UserController(dao=mockedDAO)
+
+    assert uc.get_user_by_email(email='jane.doe@email.com') == None
+
