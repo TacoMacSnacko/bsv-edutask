bd32839151cf1b083d7ca964c0afd51f4f4edcb8
Adrian Tegelind <adriantegelind@outlook.com>
2025-05-17 16:10:24 +0200

    Updated and added unit-tests based on feedback from assignment 2.

--- backend/test/unit/test_usercontroller.py
+++ backend/test/unit/test_usercontroller.py
@@ -1,7 +1,7 @@
 
 import pytest
 import unittest.mock as mock
-from unittest.mock import patch
+import sys
 from src.controllers.usercontroller import UserController
 
 
@@ -24,12 +24,22 @@ def test_get_user_by_email_1_match():
 
 
 @pytest.mark.unit
-def test_get_user_by_email_multiple_matches():
+def test_get_user_by_email_multiple_matches(capsys):
     mockedDAO = mock.MagicMock()
     mockedDAO.find.return_value = [user1, user2]
     uc = UserController(dao=mockedDAO)
+    
+    returnedUser = uc.get_user_by_email(email='jane.doe@email.com')
+    out, err = capsys.readouterr()
 
-    assert uc.get_user_by_email(email='jane.doe@email.com') == user1
+    # capsys.readouterr consumes the text from the stream, output it back into the stream.
+    sys.stdout.write(out)
+    sys.stdout.write(err)
+
+    isUser1 = returnedUser == user1
+    havePrintedEmail = 'jane.doe@email.com' in out
+
+    assert isUser1 and havePrintedEmail
 
 
 @pytest.mark.unit
@@ -42,13 +52,53 @@ def test_get_user_by_email_0_matches():
 
 
 @pytest.mark.unit
-def test_get_user_by_email_invalid_email():
+def test_get_user_by_email_invalid_email_missing_local():
     mockedDAO = mock.MagicMock()
     mockedDAO.find.return_value = []
     uc = UserController(dao=mockedDAO)
 
     with pytest.raises(ValueError):
-        uc.get_user_by_email(email='jane.doe')
+        uc.get_user_by_email(email='@email.com')
+
+
+@pytest.mark.unit
+def test_get_user_by_email_invalid_email_missing_at():
+    mockedDAO = mock.MagicMock()
+    mockedDAO.find.return_value = []
+    uc = UserController(dao=mockedDAO)
+
+    with pytest.raises(ValueError):
+        uc.get_user_by_email(email='jane.doeemail.com')
+
+
+@pytest.mark.unit
+def test_get_user_by_email_invalid_email_missing_domain():
+    mockedDAO = mock.MagicMock()
+    mockedDAO.find.return_value = []
+    uc = UserController(dao=mockedDAO)
+
+    with pytest.raises(ValueError):
+        uc.get_user_by_email(email='jane.doe@.com')
+
+
+@pytest.mark.unit
+def test_get_user_by_email_invalid_email_missing_dot():
+    mockedDAO = mock.MagicMock()
+    mockedDAO.find.return_value = []
+    uc = UserController(dao=mockedDAO)
+
+    with pytest.raises(ValueError):
+        uc.get_user_by_email(email='jane.doe@emailcom')
+
+
+@pytest.mark.unit
+def test_get_user_by_email_invalid_email_missing_host():
+    mockedDAO = mock.MagicMock()
+    mockedDAO.find.return_value = []
+    uc = UserController(dao=mockedDAO)
+
+    with pytest.raises(ValueError):
+        uc.get_user_by_email(email='jane.doe@email.')
 
 
 @pytest.mark.unit
@@ -57,7 +107,5 @@ def test_get_user_by_email_db_failure():
     mockedDAO.find.side_effect = Exception("Database error")
     uc = UserController(dao=mockedDAO)
 
-    with pytest.raises(Exception) as exc_info:
+    with pytest.raises(Exception, match= "Database error"):
         uc.get_user_by_email(email='jane.doe@email.com')
-
-    assert "Database error" in str(exc_info.value)
